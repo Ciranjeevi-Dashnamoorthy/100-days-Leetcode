f3c58d0520b4377e85458a25eb18e3e20603c9bf
Ciranjeevi Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2025-07-22 23:00:11 +0530

    Day 01 Two Sum

--- Day01/twosum.py
+++ Day01/twosum.py
@@ -1,1 +1,8 @@
-print("hello")
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num=set()
+        for i in range(len(nums)):
+         compli=target-nums[i]
+         if compli in num:
+            return [i,nums.index(compli)]
+         num.add(nums[i])
