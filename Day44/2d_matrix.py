c4c083dda9917840ab4a97fe81c40ee68f73c90f
Ciranjeevi Dashnamoorthy <ciranjeevi.dashnamoorthy@gmail.com>
2025-09-27 00:09:12 +0530

    Day 44 Serach in a 2D Matrix

---
+++ Day44/2d_matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchMatrix(selrf, matrix: List[List[int]], target: int) -> bool:
+        m,n=len(matrix),len(matrix[0])
+        l,r=0,m*n-1
+        while l<=r:
+            mid=(l+r)//2
+            num=matrix[mid//n][mid%n]
+            if num==target:
+                return True
+            elif num<target:
+                l=mid+1
+            else:
+                r=mid-1
+        return False
+        
+
+
+
+
+
+
+            
+
+
+        
